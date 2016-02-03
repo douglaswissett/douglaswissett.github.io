83f94d9d8814160586e2cae049d83af68f0e50a3
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-02-03 10:20:00 -0500

    added scoring and highscore feature

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
+//**********************************************************************
+//
+//  AUTHOR    - DOUGLAS WISSETT WALKER
+//  DATE      - 02/02/2016
+//  VERSION   - 1.0.4
+//
+//  REFERENCES: UiTutorial to help moveSnake logic 
+//
+//
+//**********************************************************************
 $(function(){
  
 
-
   var $gamebox = $('#gamebox');
+  var speed = 100;
+  var score = 0;
+  var highscore = 0;
+
   var snake = ['10_8','10_7','10_6']; // array to hold snake 
   var tail;
   var head;
-  var speed = 200;
-  var direction = 2;
-  var score = 0;
+  var direction = 'right';
+
+  // var snake2 = ['10_28','10_29','10_30']; // array to hold snake2 
+  // var tail2;
+  // var head2;
+  // var direction2 = 'left';
 
 
 
@@ -16,30 +32,50 @@ $(function(){
   $(document).keydown(function(e){
     var dir = e.keyCode;
 
+
+    // player 1 directions
     switch(dir) {
       case 38:
-        if(direction === 3) return;
-          direction = 1;
-          console.log('Up!');
+        if(direction === 'down') return;
+          direction = 'up';
         break;
       case 39:
-        if(direction === 4) return;
-        direction = 2;
-        console.log('Right!');
+        if(direction === 'left') return;
+        direction = 'right';
         break;
       case 40:
-        if(direction === 1) return;
-        direction = 3;
-        console.log('Down!');
+        if(direction === 'up') return;
+        direction = 'down';
         break;
       case 37:
-        if(direction === 2) return;
-        direction = 4;
-        console.log('Left!');
+        if(direction === 'right') return;
+        direction = 'left';
         break;
       default:
         direction;
     }
+
+    // player 2 directions
+    // switch(dir) {
+    //   case 87:
+    //     if(direction === 'down') return;
+    //       direction = 'up';
+    //     break;
+    //   case 68:
+    //     if(direction === 'left') return;
+    //     direction = 'right';
+    //     break;
+    //   case 83:
+    //     if(direction === 'up') return;
+    //     direction = 'down';
+    //     break;
+    //   case 65:
+    //     if(direction === 'right') return;
+    //     direction = 'left';
+    //     break;
+    //   default:
+    //     direction;
+    // }
   });
 
   function buildCells() {
@@ -51,54 +87,93 @@ $(function(){
   }    
 
   function buildSnake() {
-    $('#10_8').addClass('cellSnake');
-    $('#10_7').addClass('cellSnake');
-    $('#10_6').addClass('cellSnake');
+    // spawn snake 1
+    $('#10_8').addClass('snake');
+    $('#10_7').addClass('snake');
+    $('#10_6').addClass('snake');
+    // spawn snake 2
+    // $('#10_28').addClass('snake');
+    // $('#10_29').addClass('snake');
+    // $('#10_30').addClass('snake');
   }
 
   function generateFood() {
+    // generate random number between 0-29 for x & y coordinates
     var x = Math.floor(Math.random() * 29);
     var y = Math.floor(Math.random() * 29);
-
-    $('#'+ x +'_'+ y).addClass('cellFood');
+    // query generated position, add food css to that div
+    $('#'+ x +'_'+ y).addClass('food');
+    // store food's position to check for detection later
     food = x +'_'+ y;
   }
 
   function removeTail() {
+    // removes snake tail
     tail = snake.pop();
-    $('#'+tail).removeClass('cellSnake');
+    $('#'+tail).removeClass('snake');
+
+    // tail2 = snake2.pop();
+    // $('#'+tail2).removeClass('snake');
   }
 
-  function moveSnake() {
-    var oldHead = snake[0];
-    var newHead = oldHead.split('_');
-    row = +(newHead[0]);     // 10
-    col = +(newHead[1]);     // 8
 
+
+  // reference from UI tutorial
+  function moveSnake() {
+    // grab snake head, get x & y position
+    var newHead = snake[0].split('_');
+    row = +(newHead[0]);
+    col = +(newHead[1]);
+    // depending on current direction, calculate next movement
     switch(direction) {
-      case 1:
+      case 'up':
           row = row-1;
         break;
-      case 2:
+      case 'right':
         col = col+1;
         break;
-      case 3:
+      case 'down':
         row = row+1;
         break;
-      case 4:
+      case 'left':
         col = col-1;
         break;
     }
+    // add new head position to snake array
     head = row +'_'+ col;
     snake.unshift(head);
-    $('#'+head).addClass('cellSnake');
+    $('#'+head).addClass('snake');
+
+    // // grab snake head, get x & y position
+    // var newHead = snake[0].split('_');
+    // row = +(newHead[0]);
+    // col = +(newHead[1]);
+    // // depending on current direction, calculate next movement
+    // switch(direction) {
+    //   case 'up':
+    //       row = row-1;
+    //     break;
+    //   case 'right':
+    //     col = col+1;
+    //     break;
+    //   case 'down':
+    //     row = row+1;
+    //     break;
+    //   case 'left':
+    //     col = col-1;
+    //     break;
+    // }
+    // // add new head position to snake array
+    // head = row +'_'+ col;
+    // snake.unshift(head);
+    // $('#'+head).addClass('snake');
   }
 
   function checkForEat() {
     if(head === food) {
       snake.push(tail);
-      $('#'+tail).addClass('cellSnake');
-      $('#'+food).removeClass('cellFood');
+      $('#'+tail).addClass('snake');
+      $('#'+food).removeClass('food');
       generateFood();
       score++;
     }
@@ -106,11 +181,37 @@ $(function(){
 
   function checkHit() {
 
-    if(row < 0 || col < 0 || row > 30 || col > 30) {
-      console.log('dead!');
+    if(row < 0) {
       alert('You deeeeeed!');
       clearScreen();
       clearTimeout(timerId);
+      highscore = score;
+      var $message = $gamebox.append('<div id="gameMessage">'+'You scored: '+score+'</div>');
+
+      return true;
+    } else if(col < 0) {
+      alert('You deeeeeed!');
+      clearScreen();
+      clearTimeout(timerId);
+      highscore = score;
+      var $message = $gamebox.append('<div id="gameMessage">'+'You scored: '+score+'</div>');
+
+      return true;
+    } else if(row > 30) {
+      alert('You deeeeeed!');
+      clearScreen();
+      clearTimeout(timerId);
+      highscore = score;
+      var $message = $gamebox.append('<div id="gameMessage">'+'You scored: '+score+'</div>');
+
+      return true;
+    } else if(col > 30) {
+      alert('You deeeeeed!');
+      clearScreen();
+      clearTimeout(timerId);
+      highscore = score;
+      var $message = $gamebox.append('<div id="gameMessage">'+'You scored: '+score+'</div>');
+
       return true;
     }
   }
@@ -131,10 +232,12 @@ $(function(){
 
 // TODO
 // 
-// - implement self hit detection
-// - add highscore feature
-// - 2 player feature
-// - difficulty level
+// - implement self hit detection   - 
+// - add highscore feature          - fixed
+// - 2 player feature               - progressing
+// - difficulty level               - 
+//
+//
 
 
 
@@ -143,15 +246,12 @@ $(function(){
 
 
 
-
-
-
-
+  // timeout ID
   var timerId;
+  // game refresh
   function refresh() {
     game.update();
   }
-
   // game logic
   var game = {
 
@@ -167,14 +267,14 @@ $(function(){
       removeTail();
       moveSnake();
       checkForEat();
-      $('#scoreboard').text('Score: ' + score);
       checkHit();
+      $('#scoreboard').text('Highscore: ' + highscore);
 
+      // if checkHit === true, stop game
       if(checkHit()) return;
       timerId = setTimeout(refresh, speed);
     }
   };
-
   // init game
   game.startGame();  
 });
