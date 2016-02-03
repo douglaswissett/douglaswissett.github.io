6d504e0ef32af61e3a59ddebffa470007c664818
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-02-02 20:56:11 -0500

    added first implementation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 $(function(){
  
+
+
   var $gamebox = $('#gamebox');
+  var snake = ['10_8','10_7','10_6']; // array to hold snake 
+  var food;
+
+  //keydown listener
+  $(document).keydown(function(e){
+    
+    console.log(e);
+
+
+  });
+
+
+
 
 
   function buildCells() {
@@ -13,7 +28,29 @@ $(function(){
   }    
 
   function buildSnake() {
-    snake = [];
+    $('#10_8').addClass('cellSnake');
+    $('#10_7').addClass('cellSnake');
+    $('#10_6').addClass('cellSnake');
+  }
+
+  function generateFood() {
+    var x = Math.floor(Math.random() * 29);
+    var y = Math.floor(Math.random() * 29);
+
+    $('#'+ x +'_'+ y).addClass('cellFood');
+    food = $('#'+ x +'_'+ y);
+  }
+
+  function removeTail() {
+    var tail = snake.pop();
+    $('#'+tail).removeClass('cellSnake');
+  }
+
+  function animateSnake(dir) {
+
+    snake.unshift(dir);
+    $('#'+dir).addClass('cellSnake');
+    removeTail();
   }
 
 
@@ -21,23 +58,40 @@ $(function(){
 
 
 
+  function direction() {
+    var head = snake[0];
+    var newHead;                // #10_9
+
+  }
 
 
 
 
+  // clear game window 
+  function clearScreen() {
+    for(var i = 0; i < 30; i++) {
+      for(var j = 0; j < 30; j++) {
+        $('#'+i+'_'+j).removeClass();
+      }
+    }
+  }
 
-
-
+  // game logic
   var game = {
 
     startGame: function() {
       buildCells();
+      buildSnake();
+
+
+      generateFood(); // this should be called on init + when food is eaten
     }
 
   };
 
   
   game.startGame();  
+
 
 });
 
