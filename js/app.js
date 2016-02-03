86afd4d54b0670da3ba8769711044bf9121c32d4
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-02-02 19:38:49 -0500

    updated styles + grid size

--- js/app.js
+++ js/app.js
@@ -3,17 +3,18 @@ $(function(){
   var $gamebox = $('#gamebox');
 
 
-
   function buildCells() {
 
     for(var i = 0; i < 30; i++) {
       for(var j = 0; j < 30; j++) {
-        $gamebox.append('<div class="cell" id= i + "_" + j ></div>');
+        $gamebox.append('<div class="cell" id='+i+'_'+j+'></div>');
       }
     }
   }    
 
-
+  function buildSnake() {
+    snake = [];
+  }
 
 
 
